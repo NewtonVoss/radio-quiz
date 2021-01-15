d611f900497597f207e0586c92847f6e3755e3b0
NewtonVoss <77486794+NewtonVoss@users.noreply.github.com>
2021-01-15 11:13:12 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,21 +3,15 @@ let Kopp = 0
 let Produkt = 0
 input.onButtonPressed(Button.A, function () {
     Krus += 1
-    basic.showNumber(Kopp)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(Kopp * Krus)
     Produkt = Kopp * Krus
     radio.setGroup(Produkt)
 })
-input.onButtonPressed(Button.AB, function () {
-    Krus = 0
-    Kopp = 0
-})
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
     Kopp += 1
-    basic.showNumber(Krus)
 })
